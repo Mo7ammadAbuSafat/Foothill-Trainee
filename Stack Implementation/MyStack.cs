637294603ad2be4d2adff339977ff2975bf92cb2
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-11-04 21:10:16 +0200

    Stack task

---
+++ Stack Implementation/MyStack.cs
@@ -0,0 +1,76 @@
+﻿using Stack_Implementation;
+
+namespace Stack_Implementation
+{
+    public class MyStack<T>
+    {
+        class Entity
+        {
+            public Entity next;
+            public T val;
+
+            public Entity(Entity e, T val)
+            {
+                this.next = e;
+                this.val = val;
+            }
+
+        }
+
+        private Entity top;
+        private int size;
+
+        public void Push(T val)
+        {
+            top = new Entity(top, val);
+            size++;
+        }
+
+        public void Peek()
+        {
+            if (size == 0)
+            {
+                throw new ArgumentOutOfRangeException("stack is empty");
+            }
+            Console.WriteLine(top.val);
+        }
+
+        public T Pop()
+        {
+            if (size == 0)
+            {
+                throw new ArgumentOutOfRangeException("the stack is empty");
+            }
+            T val = top.val;
+            top = top.next;
+            size--;
+            return val;
+        }
+
+        public void Print()
+        {
+            if(size == 0)
+                Console.WriteLine("the stack is empty");
+            else
+            {
+                Entity current = top;
+                string s = "{ ";
+                while (current != null)
+                {
+                    s += (current.val + ", ");
+                    current = current.next;
+                }
+                Console.WriteLine(s + "}");
+            }
+            
+        }
+
+        public void Clear()
+        {
+            top = null;
+            size = 0;
+        }
+
+    }
+
+}
---
+++ Stack Implementation/RunTime.cs
@@ -0,0 +1,20 @@
+﻿using Stack_Implementation;
+
+class RunTime
+{
+    public static void Main(string[] args)
+    {
+        MyStack<int> stack = new();
+        stack.Push(1);
+        stack.Push(2);
+        stack.Push(3);
+        stack.Push(4);
+        stack.Push(5);
+        stack.Pop();
+        stack.Peek();
+        stack.Print();
+        stack.Clear(); 
+        stack.Print();
+        stack.Pop();
+    }
+}
