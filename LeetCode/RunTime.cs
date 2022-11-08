d83b7fea3c205d3b3212f5cb4bf8f318bdeeb6ec
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-11-08 18:25:23 +0200

    add Sort Problem

---
+++ LeetCode/RunTime.cs
@@ -0,0 +1,14 @@
+﻿
+using static LeetCode.Sort;
+namespace LeetCode
+{
+    class RunTime
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 58, 4, 1, 2, 2, 0 };
+            Sort.SortColors(arr);
+            Console.WriteLine(arr);
+        }
+    }
+}
