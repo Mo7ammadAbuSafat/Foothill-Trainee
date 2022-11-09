391541c1c0b03511ed17fba98d0f6fea2f786f21
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-11-10 01:44:22 +0200

    add a O(n) time complexity for Rotate problem

--- LeetCode/RotateRight.cs
+++ LeetCode/RotateRight.cs
@@ -17,5 +17,14 @@
                 nums[index++] = x;
             }
         }
+
+        public static void Rotate2(int[] nums, int k)
+        {
+            k = k % nums.Length;
+            int[] result = new int[nums.Length];
+            Array.Copy(nums, 0, result, 0, nums.Length);
+            for (int i = 0; i < nums.Length; i++)
+                nums[i] = result[(i - k + nums.Length) % (result.Length)];
+        }
     }
 }
