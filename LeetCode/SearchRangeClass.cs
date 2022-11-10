dff9277c4e46df086c4f76562c05a950b7b4ba5c
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-11-10 17:04:52 +0200

    improve SearchRangeClass

--- LeetCode/SearchRangeClass.cs
+++ LeetCode/SearchRangeClass.cs
@@ -10,5 +10,33 @@
             result[1]=list.LastIndexOf(target);
             return result;
         }
+
+        public int[] SearchRange2(int[] nums, int target)
+        {
+            int[] result = new int[] {-1,-1};
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (target == nums[mid])
+                {
+                    result[0]=result[1]=mid;
+                    break;
+                }
+                else if (target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+               
+            }
+            while (result[0] - 1 >= 0 && nums[result[0]-1] == target)
+                result[0]-=1;
+
+            while (result[1] + 1 < nums.Length && nums[result[1] + 1] == target)
+                result[1] += 1;
+
+            return result;
+        }
     }
 }
