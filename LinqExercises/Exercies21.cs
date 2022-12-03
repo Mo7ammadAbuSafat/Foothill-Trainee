1a96244c48e70d68a107c0bb84440189c350e2d2
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-12-03 12:06:51 +0200

    add Exercise24

---
+++ LinqExercises/Exercies21.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SingleCalculatedValue1
+{
+    public static class Aggregate1
+    {
+        // Return the total number of characters in all words in
+        // the source sequence
+        public static int TotalCharactersInSequence(IEnumerable<string> words)
+        {
+            // Uncomment:
+            // return words.???();
+            return words.Aggregate(0, (sum, word) => sum + word.Length);
+        }
+    }
+}
