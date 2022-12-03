94b64c75978473fc9c16f055b71db8152b68d10c
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-12-03 12:00:36 +0200

    add Exercise14

---
+++ LinqExercises/Exercise14.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MultipleValue1
+{
+    public static class SkipTake1
+    {
+        // Return the 3rd, 4th, and 5th items of the provided sequence.
+        public static IEnumerable<string> GetThirdFourthFifthItems(
+            IEnumerable<string> words)
+        {
+            // Uncomment:
+            // return words.???();
+            return words.Skip(2).Take(3);
+        }
+    }
+}
