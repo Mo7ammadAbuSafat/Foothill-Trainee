2b1ae06bfdcee15c3fb07b90d34add0a21063640
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-12-03 12:01:39 +0200

    add Exercise15

---
+++ LinqExercises/Exercise15.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MultipleValue1
+{
+    public static class Count1
+    {
+        // Return all words in the sequence between "start" (inclusive)
+        // and "end" (non-inclusive)
+        // For example, if given { "One", "start", "more", "end", "thing" } ...
+        // this method should return { "start", "more" }
+        public static IEnumerable<string> GetStartThroughEnd(IEnumerable<string> words)
+        {
+            // Uncomment:
+            // return words.???();
+            return words.SkipWhile(word => word != "start").TakeWhile(word => word != "end");
+        }
+    }
+}
