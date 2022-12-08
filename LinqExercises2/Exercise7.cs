6d97c3f81d717b2f25fe0b61d055fc29958ae77c
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-12-08 09:53:48 +0200

    add Exercise7

---
+++ LinqExercises2/Exercise7.cs
@@ -0,0 +1,21 @@
+﻿namespace ExtensionMethods2
+{
+    // Write the extension method (and containing class) here,
+    // following the example in the lesson. The method should
+    // be called MakePlural(), extends string, takes an int
+    // parameter, and returns a string.
+    //
+    // If the passed-in int is 1, then MakePlural() should
+    // return the original string, unmodified. Otherwise, it
+    // should return the string with an 's' appended to it.
+
+    // public ... class ...
+    // { }
+    public static class Extension
+    {
+        public static string MakePlural(this string s, int a)
+        {
+            return a == 1 ? s : $"{s}s";
+        }
+    }
+}
