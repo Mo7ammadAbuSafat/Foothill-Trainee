68887a9aded03ec01ebf9d4059988f16ef90c9fd
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-12-08 09:45:57 +0200

    add Exercise6

---
+++ LinqExercises2/Exercise6.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtensionMethods1
+{
+    // Write the extension method (and containing class) here,
+    // following the example in the lesson. The method should
+    // be called SayHello(), extends string, and returns a string.
+
+    // public ... class ...
+    // { }
+    public static class Extention
+    {
+        public static string SayHello(this string s)
+        {
+            return $"Hello, {s}!";
+        }
+    }
+}
